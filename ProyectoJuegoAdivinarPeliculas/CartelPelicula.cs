78844756a22c7940cc57f686614f10c1fbf5d179
SanHurt <44769252+SanHurt@users.noreply.github.com>
2021-01-15 14:00:49 +0100

    Agregar archivos de proyecto.

---
+++ ProyectoJuegoAdivinarPeliculas/CartelPelicula.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+
+namespace ProyectoJuegoAdivinarPeliculas
+{
+    public enum Dificultad {Baja, Normal, Alta }
+    class CartelPelicula : INotifyPropertyChanged
+    {
+        public CartelPelicula()
+        {
+            
+        }
+        public CartelPelicula(string nombre, string imagen, string pista, string genero, Dificultad dificultad)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Pista = pista;
+            Genero = genero;
+            Dificultad = dificultad;
+        }
+        private string _nombre;
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set
+            {
+                if (this._nombre != value)
+                {
+                    this._nombre = value;
+                    this.NotifyPropertyChanged("Nombre");
+                }
+            }
+        }
+
+        private string _imagen;
+        public string Imagen
+        {
+            get { return this._imagen; }
+            set
+            {
+                if (this._imagen != value)
+                {
+                    this._imagen = value;
+                    this.NotifyPropertyChanged("Imagen");
+                }
+            }
+        }
+        private string _pista;
+        public string Pista
+        {
+            get { return this._pista; }
+            set
+            {
+                if (this._pista != value)
+                {
+                    this._pista = value;
+                    this.NotifyPropertyChanged("Pista");
+                }
+            }
+        }
+        private string _genero;
+        public string Genero
+        {
+            get { return this._genero; }
+            set
+            {
+                if (this._genero != value)
+                {
+                    this._genero = value;
+                    this.NotifyPropertyChanged("Género");
+                }
+            }
+        }
+        private Dificultad _dificultad;
+        public Dificultad Dificultad
+        {
+            get { return this._dificultad; }
+            set
+            {
+                if(this._dificultad != value)
+                {
+                    this._dificultad = value;
+                    this.NotifyPropertyChanged("Dificultad");
+                }
+            }
+        }
+        public static ObservableCollection<CartelPelicula> GetSamples(string ruta)
+        {
+            ObservableCollection<CartelPelicula> lista = new ObservableCollection<CartelPelicula>();
+
+            lista.Add(new CartelPelicula("Pulp Fiction", Path.Combine(ruta, @"pulp_fiction.png"), "Pulp Fiction", "Thriller", Dificultad.Baja));
+            lista.Add(new CartelPelicula("Miedo y asco en Las Vegas", Path.Combine(ruta, @"miedo_y_asco.jpg"), "Miedo y...", "Novela", Dificultad.Baja));
+            lista.Add(new CartelPelicula("Spiderman", Path.Combine(ruta, @"spiderman.jpg"), "Spiderman", "Ciencia-ficción", Dificultad.Baja));
+
+            return lista;
+        }
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ ProyectoJuegoAdivinarPeliculas/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace ProyectoJuegoAdivinarPeliculas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<CartelPelicula> listaPeliculas;
+        private int numActualCartel = 1;
+        public enum CambiarCartel
+        {
+            Siguiente = 1,
+            Anterior = -1,
+            Actual = 0
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            listaPeliculas = CartelPelicula.GetSamples(@"F:\DAM\2DAM\DINT\UT5\RecursosProyecto\Assets\");
+            listBox.DataContext = listaPeliculas;
+            ActualizaLista((int)CambiarCartel.Actual);
+        }
+        private void ActualizaLista(CambiarCartel cartel)
+        {
+            numActualCartel += (int)cartel;
+            numeroCartel.Text = numActualCartel + " / " + listaPeliculas.Count;
+            dockPanelPelicula.DataContext = listaPeliculas[numActualCartel - 1];
+        }
+        private void AddCartelPelicula(CartelPelicula c)
+        {
+            listaPeliculas.Add(c);
+            MessageBox.Show("Cartel añadido");
+        }
+
+        private void Anterior_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (1 < numActualCartel)
+            {
+                
+                ActualizaLista(CambiarCartel.Anterior);
+                dockPanel.DataContext = listaPeliculas[numActualCartel - 1];
+            }
+                
+        }
+
+        private void Siguiente_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (numActualCartel < listaPeliculas.Count)
+            {
+                
+                ActualizaLista(CambiarCartel.Siguiente);
+                dockPanel.DataContext = listaPeliculas[numActualCartel - 1];
+            }
+                
+            
+        }
+
+        private void buttonValidar_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxAdivinarPelicula.Text == listaPeliculas[numActualCartel - 1].Nombre)
+                MessageBox.Show("¡Has acertado!");
+            else
+                MessageBox.Show("¡Has perdido!");
+        }
+
+        private void checkBoxVerPista_Checked(object sender, RoutedEventArgs e)
+        {
+            textBlockPista.Visibility = Visibility.Visible;
+        }
+
+        private void checkBoxVerPista_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBlockPista.Visibility = Visibility.Collapsed;
+        }
+    }
+}
